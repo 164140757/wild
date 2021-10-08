02ae6c35ec085827d5abc3a01ae8518839fd9019
Haotian Bai <164140757@qq.com>
2021-10-08 14:41:20 +0800

    Polish dcm2niix.

--- projects/AMOS/AMOS/data/dcm2nii.py
+++ projects/AMOS/AMOS/data/dcm2nii.py
@@ -4,32 +4,38 @@ from multiprocessing import Pool
 
 from tqdm import tqdm
 
-data_root = "/mnts2d/med_data1/haotian/AMOS/second_round/CT/2021/202101/20210118-20210131"
-out_dir = "/mnts2d/med_data1/haotian/AMOS/second_round/ct_nii"
+data_root = r"F:\MIA\AMOS-CT-MR\raw\test"
+out_dir = r"F:\MIA\AMOS-CT-MR\processed\test"
 
-def dcm2nii(dir):
-    idx_s = dir.find('Images-result-deidentify')+len('Images-result-deidentify/')
-    idx_end = idx_s+len('0b0548ed1ef4e06e4972fbf9c8946fda')
-    out_path = os.path.join(out_dir, 'ct_nii_20210118-20210131', dir[idx_s:idx_end])
+def hasSubdir(root):
+    sub_dirs = set([os.path.dirname(p) for p in glob(root+"/*/*")])
+    return len(sub_dirs)>0
+
+def dcm2nii(_dir):
+    check_id = os.path.split(_dir)[-1]
+    out_path = os.path.join(out_dir, check_id)
     os.makedirs(out_path, exist_ok=True)
-    cmd='dcm2niix -f %f_%k_%j -z y -o \"{}\" \"{}\"'.format(out_path,dir)
+    cmd='dcm2niix -f %f_%k_%j -z y -o \"{}\" \"{}\"'.format(out_path, _dir)
     res=os.popen(cmd)
     output_str=res.read()
     return output_str
 
 data_roots=glob(data_root+'/*/')
+
 total_dir=[]
-dir_list=[]
+
 totolen=0
 for data_root in data_roots:
     dir_list=[]
-    for root, subdirs, files in os.walk(data_root):
+    for root, subdirs, _ in os.walk(data_root):
         for subdir in subdirs:
             dir_list.append(os.path.join(root, subdir))
     totolen += len(dir_list)
     total_dir.extend(dir_list)
-    print(data_root + "      : " + str(len(dir_list)))
 
-print(f'Toal number of cases: {len(total_dir)}')
-with Pool(8) as p:
+# clean sub-roots
+total_dir = [x for x in total_dir if not hasSubdir(x)]
+print(f'Total number of cases: {len(total_dir)}')
+
+with Pool(3) as p:
     r=list(tqdm(p.map(dcm2nii, total_dir), total=len(total_dir)))
