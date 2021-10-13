b1663d6b53d30037f2b6b835bf6fce34d558b6a9
Haotian Bai <164140757@qq.com>
2021-10-13 18:22:34 +0800

    select interest patients

---
+++ projects/AMOS/AMOS/data/selectInterest.py
@@ -0,0 +1,24 @@
+from glob import glob
+import pandas as pd 
+
+df_path = '/mnts2d/med_data1/haotian/AMOS/second_round/CT/2021/202101/secondround_ct_data_meta_202101.xlsx'
+DATA_ROOT = r'F:\MIA\AMOS-CT-MR\processed\second_round\ct_nii\ct_nii_20210118-20210131'
+
+df = pd.read_excel(df_path, usecols=['complete_ab_flag', 'nii_file', '临床诊断', 'shape', 'Protocol Name', 'spacing', '检查时间'])
+
+df['检查时间'] = pd.to_datetime(df['检查时间'], format='%Y%m%d')
+
+df = df.loc[(df['检查时间'] >= '2021-01-18') & (df['检查时间'] <= '2021-01-31')] 
+df = df[df['complete_ab_flag']!=1]
+df = df.loc[(df['临床诊断'].str.contains('癌')) | (df['临床诊断'].str.contains('肿瘤'))]
+
+# spacing * shape -> physical distance
+spacing_z = df['spacing'].str.strip('[]').str.split(', ', expand=True).loc[:, 0].astype(float)
+shape_z = df['shape'].str.strip('()').str.split(', ', expand=True).loc[:, 0].astype(float)
+distance_z = spacing_z.multiply(shape_z, fill_value=0)*0.1
+df.insert(0, 'd_z', distance_z)
+
+df = df[df['d_z'] >= 40]
+print(f'Patients in interest: {df.shape[0]}')
+
+data_roots=glob(DATA_ROOT+'/*/')
